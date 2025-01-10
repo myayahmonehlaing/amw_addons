374871eccaff063e9212f85ba530d458dd05a567
Aung Moe Wai <84524380+AungMoeWai1@users.noreply.github.com>
2025-01-10 16:58:42 +0630

    Add files via upload
    Description: Automate Sale process to paid
    Feature:
    	1. Sale Order and Paid Button in sale order form
    	2. Automate Sale Order menuitem in the sale order tree
    	
    Detail:
    	1> Sale Order and Paid Button in Sale order form
    		-Button Will show in the draft state only
    		-Firstly you must need to fill the field of the journal(bank/cash ...etc)
    		-Cliked the button and then sale quotation will change to sale order. Need to validate in delivery. Create Invoice and paid with the selected journal.
    		
    	2> Automate Sale Order menuitem in the sale order tree
    		-Action menu will show when you selected one or more line in the sale order tree
    		-The action will work in all state you created for the sale order. it will carry to finish to paid done and need to validate in delivery.
    		-If you do for the paid, it will not change to any more.

---
+++ automate_sale_modified_v17/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Sales",
+    "author": "Aungmoewai",
+    "installed_version": "1.0",
+    "category": "Sales",
+    "license": 'LGPL-3',
+    "summary": """
+    When Sale Order is created, quantity check on warehouse: 
+    if low quantity, show the pop up box with low stock exception.
+    else stage changes to Done by passing the require setp automatically and reducing the stock from warehouse.
+    """,
+    "depends": ["base", "sale"],
+    "data": [
+        "views/ir_action_data.xml",
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
